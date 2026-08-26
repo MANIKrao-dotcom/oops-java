807a6e467363142b94294e85bd4ebec4fa1d46e2
manik rao <manikrao943@gmail.com>
2026-08-27 00:11:52 +0530

    Add files via upload

---
+++ operation5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class operation5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+
+        System.out.println("You entered: " + a);
+
+        sc.close();
+    }
+}
