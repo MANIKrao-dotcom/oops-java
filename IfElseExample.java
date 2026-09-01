a74b2450e0cd3b3940f2eb5e0fd5e30744f2cef5
manik rao <manikrao943@gmail.com>
2026-09-01 23:18:07 +0530

    Add files via upload

--- IfElseExample.java
+++ IfElseExample.java
@@ -1,11 +1,19 @@
-public class IfElseExample {
+class AgeCategory {
     public static void main(String[] args) {
-        int number = 9;
 
-        if (number % 2 == 0) {
-            System.out.println("Even number");
-        } else {
-            System.out.println("Odd number");
-        }
+        int age = 25;
+
+        if (age <= 13)
+            System.out.println("child");
+        else if (age <= 18)
+            System.out.println("teen");
+        else if (age <= 40)
+            System.out.println("major");
+        else if (age <= 60)
+            System.out.println("matured");
+        else if (age > 60)
+            System.out.println("seniour citizen");
+        else
+            System.out.println("baby");
     }
 }
