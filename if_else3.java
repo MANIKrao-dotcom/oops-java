b21723a9bbed14aa35f2dc149046e30293547744
manik rao <manikrao943@gmail.com>
2026-09-02 08:45:06 +0530

    Add files via upload

---
+++ if_else3.java
@@ -0,0 +1,12 @@
+public class if_else3 {  public static void main(String[] args) {
+        int marks = 45;
+
+        if (marks >= 35) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
+    
+
