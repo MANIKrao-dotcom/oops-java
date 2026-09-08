9d77e7abf485031e894791a0dbb940ea1e3e8c84
manik rao <manikrao943@gmail.com>
2026-09-08 11:20:35 +0530

    Add files via upload

---
+++ number_21.java
@@ -0,0 +1,17 @@
+public class number_21 {
+    public static void main(String[] args) {
+        int num = 21;
+
+        while (num != 1) {
+            System.out.println(num);
+
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num * 3 + 1;
+            }
+        }
+
+        System.out.println(num);
+    }
+}
