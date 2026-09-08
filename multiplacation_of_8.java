6a4379e605e42b981e01d1e8596d82f5b45cb589
manik rao <manikrao943@gmail.com>
2026-09-08 11:19:31 +0530

    Add files via upload

---
+++ multiplacation_of_8.java
@@ -0,0 +1,9 @@
+public class multiplacation_of_8 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("8 x " + i + " = " + (8 * i));
+        }
+
+    }
+}
