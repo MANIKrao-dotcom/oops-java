7ea25a3c209e69cb550af7934beb3c751efcc320
manik rao <manikrao943@gmail.com>
2026-09-08 11:21:23 +0530

    Add files via upload

---
+++ sum_of_numbers.java
@@ -0,0 +1,18 @@
+public class sum_of_numbers {
+    public static void main(String[] args) {
+        int n = 1234;
+
+        int a = n / 1000;
+        int b = (n / 100) % 10;
+        int c = (n / 10) % 10;
+        int d = n % 10;
+
+        int sum = a + b + c + d;
+
+        System.out.println("Sum of digits = " + sum);
+    
+
+}
+}
+
+
