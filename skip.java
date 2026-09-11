6b9d3b88c0b7f8df05d267027e32cc380552eed8
manik rao <manikrao943@gmail.com>
2026-09-11 23:21:52 +0530

    Add files via upload

---
+++ skip.java
@@ -0,0 +1,17 @@
+public class skip{   
+     public static void main(String[] args) {
+
+        int x = 1;
+
+        while (x <= 10) {
+
+            if (x == 6) {
+                break;
+            }
+
+            System.out.println(x);
+            x++;
+        }
+    }
+    
+}
