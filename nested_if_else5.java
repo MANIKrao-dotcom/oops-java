7aa8448eb90358379c7897f835f627dbd29e45e9
manik rao <manikrao943@gmail.com>
2026-09-12 23:42:51 +0530

    Add files via upload

---
+++ nested_if_else5.java
@@ -0,0 +1,19 @@
+public class nested_if_else5 {
+        public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 35) {
+
+            if (marks >= 60) {
+                System.out.println("First Class");
+            } else {
+                System.out.println("Pass");
+            }
+
+        } else {
+            System.out.println("Fail");
+        }
+    }
+    
+}
