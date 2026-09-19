93148d2a30fa779ca8277ea4a7637474389bd861
MANIKRAO <manikrao943@gmail.com>
2026-09-19 23:42:14 +0530

    Add files via upload

---
+++ switch6.java
@@ -0,0 +1,51 @@
+public class switch6{
+     public static void main(String[] args) {
+
+        int department = 1;
+        int year = 2;
+
+        switch (department) {
+
+            case 1:
+                System.out.println("Computer Science");
+
+                switch (year) {
+                    case 1:
+                        System.out.println("First Year");
+                        break;
+
+                    case 2:
+                        System.out.println("Second Year");
+                        break;
+
+                    case 3:
+                        System.out.println("Third Year");
+                        break;
+
+                    default:
+                        System.out.println("Invalid Year");
+                }
+                break;
+
+            case 2:
+                System.out.println("Mechanical");
+
+                switch (year) {
+                    case 1:
+                        System.out.println("First Year");
+                        break;
+
+                    case 2:
+                        System.out.println("Second Year");
+                        break;
+
+                    default:
+                        System.out.println("Invalid Year");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid Department");
+        }
+    }
+}
