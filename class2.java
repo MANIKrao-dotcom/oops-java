ec9cb718a3cfda1b455057a7e7fd7682caf4e953
MANIKRAO <manikrao943@gmail.com>
2026-09-21 20:38:46 +0530

    Add files via upload

---
+++ class2.java
@@ -0,0 +1,14 @@
+class Calculator2 {
+
+    void add() {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum = " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        Calculator2 c = new Calculator2();
+        c.add();
+    }
+}
