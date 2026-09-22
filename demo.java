4e7ed7516a70eba5b229f3645f3ee6c24779db28
MANIKRAO <manikrao943@gmail.com>
2026-09-22 11:13:53 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,14 @@
+class Demo {
+
+    static void add() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        System.out.println("Addition = " + c);
+    }
+
+    public static void main(String[] args) {
+        add();
+    }
+}
