71522bf74bb9947e5642c7fec91ef6e68c63679d
MANIKRAO <manikrao943@gmail.com>
2026-09-23 23:42:56 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square {
+
+    static void findSquare(int n) {
+        int result = n * n;
+        System.out.println("Square = " + result);
+    }
+
+    public static void main(String[] args) {
+        findSquare(6);
+    }
+}
