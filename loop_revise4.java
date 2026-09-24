9c0522527cb0e38a0fefe988587516726a40e32c
MANIKRAO <manikrao943@gmail.com>
2026-09-24 21:48:19 +0530

    Add files via upload

---
+++ loop_revise4.java
@@ -0,0 +1,14 @@
+public class loop_revise4 {
+     public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Table of " + i);
+
+            for(int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+
+            System.out.println();
+        }
+    } 
+}
